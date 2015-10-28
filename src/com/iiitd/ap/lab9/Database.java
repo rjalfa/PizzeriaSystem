955016889b1dc1a164201b0ba7cb74f082e1351f
Rounaq <rounaqwl66@gmail.com>
2015-10-28 16:51:13 +0530

    Added Packages and Database JAVA File

---
+++ src/com/iiitd/ap/lab9/Database.java
@@ -0,0 +1,10 @@
+package com.iiitd.ap.lab9;
+import java.util.ArrayList;
+
+import com.iiitd.ap.lab9.model.Customer;
+import com.iiitd.ap.lab9.model.Order;
+
+public class Database {
+	ArrayList<Customer> users;
+	ArrayList<Order> orders;
+}
--- src/com/iiitd/ap/lab9/servlets/NewOrder.java
+++ src/com/iiitd/ap/lab9/servlets/NewOrder.java
@@ -1,4 +1,4 @@
-
+package com.iiitd.ap.lab9.servlets;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
