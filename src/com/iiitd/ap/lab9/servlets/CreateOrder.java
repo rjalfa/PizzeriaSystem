81a962b85fe9b67d8a1e2506d176ba6db5f62fb7
Shrey <shrey14099@iiitd.ac.in>
2015-10-29 23:22:41 +0530

    Update CreateOrder + Added FinalPage Servlet

--- src/com/iiitd/ap/lab9/servlets/CreateOrder.java
+++ src/com/iiitd/ap/lab9/servlets/CreateOrder.java
@@ -11,7 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.iiitd.ap.lab9.Database;
 import com.iiitd.ap.lab9.model.Order;
+import com.iiitd.ap.lab9.model.Pizza;
 
 /**
  * Servlet implementation class NewOrder
@@ -20,6 +22,13 @@ import com.iiitd.ap.lab9.model.Order;
 public class CreateOrder extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     private static final String[] pizzas = {"mg","pep","fh","dd"};
+    private static HashMap<String,String> pizza_names = new HashMap<>();
+    static{
+    	pizza_names.put("mg", "Margherita");
+    	pizza_names.put("pep", "Pepperoni Pizza");
+    	pizza_names.put("fh", "Farmhouse");
+    	pizza_names.put("dd", "Monster Deep Dish");
+    }
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -30,34 +39,39 @@ public class CreateOrder extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response,Order order) throws ServletException, IOException {
+		response.getWriter().println("<h1>Order ID:" + order.getId() + "</h1>");
+		response.getWriter().println("<h4>Please enter delivery details</h4>");
+		response.getWriter().println("<form method='post' action='./FinalPage'>");
+		response.getWriter().println("Name: <input type='text' name='name'/>");
+		response.getWriter().println("Phone: <input type='text' name='phone'/>");
+		response.getWriter().println("Address: <input type='text' name='address'/>");
+		response.getWriter().println("<input type='submit' name='Enter details'/>");
+		response.getWriter().println("</form>");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//Order order = new Order();
 		Order order = (Order) request.getSession().getAttribute("order");
 		order = createOrder(order,request,response);
 		request.getSession().setAttribute("order",order);
-		doGet(request, response);
+		doGet(request, response,order);
 	}
 	
 	protected Order createOrder(Order order,HttpServletRequest req,HttpServletResponse response) throws IOException
 	{
 		HashMap<String,String[]> order_details = new HashMap<>();
-		String[] temp = new String[2];
+		String[] temp = new String[3];
 		for(String pizza : this.pizzas)
 		{
 			if(req.getParameter(pizza)!=null && Integer.parseInt(req.getParameter(pizza)) > 0)
 			{
 				temp[0] = req.getParameter(pizza + "-size");
 				temp[1] = req.getParameter(pizza);
-				order_details.put(pizza,temp); // {pizza_name : [size,quantity]}
-				response.getWriter().println(order_details.get(pizza)[0]);
+				temp[2] = pizza_names.get(pizza);
+				order_details.put(pizza,temp);
 			}
 		}
-		//order_details is the hash map with the order details extracted from the params
-		return order;
+		return Database.createOrder(order, order_details);
 	}
 	
 	
